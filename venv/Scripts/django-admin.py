f0415873ab0d74cffef7f1068498ec0c34961201
sksoumik <sadmanks@gmail.com>
2019-07-20 23:05:22 +0600

    share pix

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sadma\djangoprojects\bookmark_application\share_pix\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
